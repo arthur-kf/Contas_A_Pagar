dbe095be0576550f38adf3c997fcd0337c866613
arthur <arthurkennedy750@gmail.com>
2001-01-01 00:34:14 -0300

    Listagem de Presços Comcluida

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-let SuperMercado = document.getElementById("S")
-let p1 = Number(SuperMercado.value)
-let valores = []
+let SuperMercado = document.getElementById("SM")
 let lista = document.getElementById("lista")
-
-
 let res = document.getElementById("res")
+let valores = []
 
 
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function isLista(n , l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
 
 function adicionar(){
+    
     valores.push(Number(SuperMercado.value))
     let item = document.createElement("option")
     item.text = `Valor ${SuperMercado.value} adicionado`
     lista.appendChild(item)
-
+    res.innerHTML = ""
+    
 }
