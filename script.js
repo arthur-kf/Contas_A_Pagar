cd7028362b4778bddcf822afffbeb6bf902e5924
arthur <arthurkennedy750@gmail.com>
2001-01-01 01:02:02 -0300

    Soma dos Presços, correção de bugs, avisos de erros, listagem de preços concluídas

--- script.js
+++ script.js
@@ -3,29 +3,45 @@ let lista = document.getElementById("lista")
 let res = document.getElementById("res")
 let valores = []
 
-
-function isNumero(n){
-    if(Number(n) >= 1 && Number(n) <= 100){
-        return true
-    }else{
-        return false
-    }
-}
-
-function isLista(n , l){
-    if(l.indexOf(Number(n)) != -1){
-        return true
-    }else{
-        return false
-    }
-}
-
 function adicionar(){
     
-    valores.push(Number(SuperMercado.value))
-    let item = document.createElement("option")
-    item.text = `Valor ${SuperMercado.value} adicionado`
-    lista.appendChild(item)
-    res.innerHTML = ""
     
+    if(SuperMercado.value == 0){
+        window.alert("Digite um Número Válido")
+        item.text = ""
+    }else{
+        valores.push(Number(SuperMercado.value))
+        let item = document.createElement("option")
+        item.text = `R$${SuperMercado.value}`
+        lista.appendChild(item)
+        SuperMercado.value = ""
+    }
+    
+}
+
+function Calcular(){
+    if(valores.length == 0){
+        window.alert("Adicione valores para finalizar!")
+    }else{
+        let maior = valores[0]
+        let menor = valores[0]
+        let total = valores.length
+        let soma = 0
+        let media = 0
+
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+            maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / total
+
+        res.innerHTML = ""
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>A soma dos valores é ${soma}.</p>`
+        res.innerHTML += `<p>A media entre os valores é ${media}.</p>`
+    
+    }
 }
