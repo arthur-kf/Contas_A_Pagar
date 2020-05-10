cbf8242606c19bc3d1cdaa38fe669982ea2e4cf9
Arthur <arthurkennedy750@gmail.com>
2020-05-10 10:57:17 -0300

    Novo Estilo

--- script.js
+++ script.js
@@ -23,25 +23,23 @@ function Calcular(){
     if(valores.length == 0){
         window.alert("Adicione valores para finalizar!")
     }else{
-        let maior = valores[0]
-        let menor = valores[0]
         let total = valores.length
         let soma = 0
-        let media = 0
 
         for(let pos in valores){
-            soma += valores[pos]
-            if(valores[pos] > maior)
-            maior = valores[pos]
-            if(valores[pos] < menor)
-            menor = valores[pos]
+            soma += valores[pos]    
         }
-        media = soma / total
-
+        
         res.innerHTML = ""
         res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
         res.innerHTML += `<p>A soma dos valores é ${soma}.</p>`
-        res.innerHTML += `<p>A media entre os valores é ${media}.</p>`
-    
+        
+
+        
+        //res.innerHTML += `<p>A media entre os valores é ${media}.</p>`
+        //media = soma / total
+        //let maior = valores[0]
+        //let menor = valores[0]
+        //let media = 0
     }
 }
