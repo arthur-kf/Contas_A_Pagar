fe87945b1668cb93612888495cf5d160152bf95d
Arthur <arthurkennedy750@gmail.com>
2020-05-13 21:43:46 -0300

    Media De Gastos

--- scripts/script_Media_De_Gastos.js
+++ scripts/script_Media_De_Gastos.js
@@ -22,16 +22,63 @@ function semana(){
 
         let somas = [soma1 + soma2 + soma3]
         let totais = [valores1.length + valoresP.length + valoresO.length]
-        let media = somas / totais
+        let media = somas / 4
 
         campo_media.value = "R$" + media
 
     }
 }
 function mes(){
-    campo_media.value = ""
-}
+    if(valores1.length == 0 & valoresP.length == 0 & valoresO == 0){
+        window.alert("Valores Invalidos!")
+    }else{
+    
+        let soma1 = 0
+        let soma2 = 0
+        let soma3 = 0
 
+        for(let pos in valores1){
+            soma1 += valores1[pos]
+        }
+        for(let pos in valoresP){
+            soma2 += valoresP[pos]
+        }
+        for(let pos in valoresO){
+            soma3 += valoresO[pos]
+        }
+
+        let somas = [soma1 + soma2 + soma3]
+        let totais = [valores1.length + valoresP.length + valoresO.length]
+        let media = somas / 12
+
+        campo_media.value = "R$" + media
+
+    }
+}
 function ano(){
-    campo_media.value = ""
+    if(valores1.length == 0 & valoresP.length == 0 & valoresO == 0){
+        window.alert("Valores Invalidos!")
+    }else{
+    
+        let soma1 = 0
+        let soma2 = 0
+        let soma3 = 0
+
+        for(let pos in valores1){
+            soma1 += valores1[pos]
+        }
+        for(let pos in valoresP){
+            soma2 += valoresP[pos]
+        }
+        for(let pos in valoresO){
+            soma3 += valoresO[pos]
+        }
+
+        let somas = [soma1 + soma2 + soma3]
+        let totais = [valores1.length + valoresP.length + valoresO.length]
+        let media = somas / 60
+
+        campo_media.value = "R$" + media
+
+    }
 }
